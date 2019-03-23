d2bd81beb23f1f974e1e3e48274362c6de36fbe1
dianalimnos <dianalim1117@gmail.com>
2019-03-23 11:48:53 -0700

    implementing cookie in app.js

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 require('dotenv').config();
@@ -23,7 +26,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true }));
+
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
