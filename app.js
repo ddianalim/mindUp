20ed69f17c2311502c7ee3edd9f720cf69f4e4ae
natalielim <natalielim@gmail.com>
2019-03-23 12:23:21 -0700

    removed daysRouter

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var daysRouter = require('./routes/days');
+// var daysRouter = require('./routes/days');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/days', daysRouter);
+// app.use('/days', daysRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
